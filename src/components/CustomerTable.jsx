3393170431ae680e314e47b5577e1d1c3ee16749
pavan6cs <bhavanas904@gmail.com>
2023-09-29 23:09:28 +0530

    feat: search functionality

--- src/components/CustomerTable.jsx
+++ src/components/CustomerTable.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
-import  {Table} from 'antd';
-import { customersData } from '../data/customersData';
+import React, { useState } from "react";
+import { Table } from "antd";
+import { customersData } from "../data/customersData";
 
-const {Column, columnGroup} = Table;
+const { Column, columnGroup } = Table;
 
 const CustomerTable = () => {
-  
+  const [customerData, setCustomerData] = useState(customersData);
+
+  const handleChange = (e) => {
+    const searchData = e.target.value;
+    const searchedData = searchData.length
+      ? customersData.filter((elem) =>
+          elem.name.toLowerCase().includes(searchData.toLowerCase())
+        )
+      : customersData;
+    setCustomerData(searchedData);
+  };
+
   return (
-    <div style={{marginTop: "5rem"}}>
-        <h3>Active Members</h3>
-        <Table dataSource={customersData}
-        pagination={{ defaultPageSize: 6 }}
-        >
+    <div style={{ marginTop: "5rem" }}>
+      <h3>Active Members</h3>
+
+      <input type="search" onChange={handleChange} />
+
+      <Table dataSource={customerData} pagination={{ defaultPageSize: 6 }}>
         <Column title="Customer Name" dataIndex="name" key="name" />
         <Column title="Company" dataIndex="company" key="company" />
-        <Column title="Phone Number" dataIndex="phoneNumber" key="phoneNumber" />
+        <Column
+          title="Phone Number"
+          dataIndex="phoneNumber"
+          key="phoneNumber"
+        />
         <Column title="Email" dataIndex="email" key="email" />
         <Column title="Country" dataIndex="country" key="country" />
         <Column title="Status" dataIndex="status" key="status" />
-        </Table>
+      </Table>
     </div>
-  )
-}
+  );
+};
 
 export default CustomerTable;
--- src/data/customersData.js
+++ src/data/customersData.js
@@ -95,4 +95,100 @@ export const customersData = [
         country: "Israel" ,
         status: "Inactive"
     },
+    {
+        name: "Jane Cooper",
+        company: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        country: "United States " ,
+        status: "Active"
+    },
+    {
+        name: "Floyd Miles",
+        company: "Yahoo",
+        phoneNumber: "(205) 555-0100" ,
+        email: "floyd@yahoo.com" ,
+        country: "Kiribati" ,
+        status: "Inactive"
+    },
+    {
+        name: "Ronald Richards",
+        company: "Adobe",
+        phoneNumber: "(302) 555-0107",
+        email: "ronald@adobe.com",
+        country: "Israel" ,
+        status: "Inactive"
+    },
+    {
+        name: " Marvin McKinney",
+        company: "Tesla",
+        phoneNumber: "(252) 555-0126" ,
+        email: "marvin@tesla.com",
+        country: "Iran" ,
+        status: "Active"
+    },
+    {
+        name: "Jerome Bell",
+        company: "Google",
+        phoneNumber: "(629) 555-0129",
+        email: "jerome@google.com" ,
+        country: "Reunion",
+        status: "Active"
+    },
+    {
+        name: "Kathyrn Murphy",
+        company: "Microsoft",
+        phoneNumber: "(406) 555-0120 ",
+        email: "kathryn@microsoft.com" ,
+        country: "Curaçao" ,
+        status: "Active"
+    },
+    {
+        name: " Marvin McKinney",
+        company: "Tesla",
+        phoneNumber: "(252) 555-0126" ,
+        email: "marvin@tesla.com",
+        country: "Iran" ,
+        status: "Active"
+    },
+    {
+        name: "Jerome Bell",
+        company: "Google",
+        phoneNumber: "(629) 555-0129",
+        email: "jerome@google.com" ,
+        country: "Reunion",
+        status: "Active"
+    },
+    {
+        name: "Kathyrn Murphy",
+        company: "Microsoft",
+        phoneNumber: "(406) 555-0120 ",
+        email: "kathryn@microsoft.com" ,
+        country: "Curaçao" ,
+        status: "Active"
+    },
+    {
+        name: "Jane Cooper",
+        company: "Microsoft",
+        phoneNumber: "(225) 555-0118",
+        email: "jane@microsoft.com",
+        country: "United States " ,
+        status: "Active"
+    },
+    {
+        name: "Floyd Miles",
+        company: "Yahoo",
+        phoneNumber: "(205) 555-0100" ,
+        email: "floyd@yahoo.com" ,
+        country: "Kiribati" ,
+        status: "Inactive"
+    },
+    {
+        name: "Ronald Richards",
+        company: "Adobe",
+        phoneNumber: "(302) 555-0107",
+        email: "ronald@adobe.com",
+        country: "Israel" ,
+        status: "Inactive"
+    }
 ]
